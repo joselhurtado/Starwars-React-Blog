2b868260675dbbd17e98b6627c42aa341ee0d7c4
Jose Hurtado <hello@josehurtado.net>
2021-01-21 03:54:53 +0000

    scroll on cards

--- src/js/component/charactersCard.js
+++ src/js/component/charactersCard.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 //characterCards
 export function CharactersCard(props) {
 	return (
-		<div className="col-3">
+		<div className="col-3 mb-2">
 			<img
 				className="card-img-top"
 				src="https://starwarsblog.starwars.com/wp-content/uploads/2019/08/rise-of-skywalker-poster-tall.jpg"
@@ -15,7 +15,7 @@ export function CharactersCard(props) {
 
 			{props.character.name ? (
 				<div className="card-body">
-					<h5 className="card-title">{props.character.name}</h5> <br />
+					<h5 className="card-title">{props.character.name}</h5>
 					<p className="card-text">
 						Hair Color: {props.character.hair_color} <br />
 						Height: {props.character.height} <br />
