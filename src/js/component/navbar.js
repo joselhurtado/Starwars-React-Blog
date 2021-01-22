6cb7d82f7122024b9da0da9948cca80643911c31
Jose Hurtado <hello@josehurtado.net>
2021-01-22 04:10:06 +0000

    NavBar edits

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-dark bg-dark mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">
-					<img
-						src="https://cutewallpaper.org/21/stars-wars-logo/List-of-Star-Wars-films-Wikipedia.png"
-						width="auto"
-						height="80px"
-					/>
-				</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-warning">Check the Context in action</button>
+		<nav className="navbar navbar-dark bg-dark" collapseOnSelect>
+			<div className="container-fluid" id="responsive-navbar-nav">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">
+						<img
+							src="https://cutewallpaper.org/21/stars-wars-logo/List-of-Star-Wars-films-Wikipedia.png"
+							width="auto"
+							height="80px"
+						/>
+					</span>
 				</Link>
+				<form className="d-flex">
+					<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+					<Link to="/demo">
+						<button className="btn btn-warning">Favorites</button>
+					</Link>
+				</form>
 			</div>
 		</nav>
 	);
